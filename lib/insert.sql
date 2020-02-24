ec42be3df8a42e7d31320823825303519ae1726f
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 02:10:12 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,6 +1,6 @@
 
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Lord of the Rings", 1, 1);
-INSERT INTO series (title, author_id, subgenre_id) VALUES ("Harry Potter", 2, 2);
+INSERT INTO series (title, author_id, subgenre_id) VALUES
+("Lord of the Rings", 1, 1), ("Harry Potter", 2, 2);
 
 INSERT INTO subgenres (name) VALUES ("Fantasy");
 INSERT INTO subgenres (name) VALUES ("Magic");
