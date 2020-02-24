2e1ceda0d5deff7a8eab136c61f05d43ba701275
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 02:27:20 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -29,15 +29,15 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT title FROM (
-    SELECT series.title AS title, COUNT(characters.series_id) AS num_species
-      FROM series
-      INNER JOIN characters
-        ON series.id = characters.series_id
-      WHERE characters.species = 'human'
-      GROUP BY characters.series_id
-      ORDER BY num_species DESC
-      LIMIT 1);"
+  "SELECT series.title FROM series 
+    JOIN books ON books.series_id = series.id 
+    JOIN character_books 
+      ON character_books.book_id = books.id 
+    JOIN characters 
+      ON character_books.character_id = characters.id 
+      WHERE characters.species = 'human' 
+      GROUP BY series.title 
+      ORDER BY COUNT(*) DESC LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
