54c7c5d12414f4ee3b3e42edcc078237781a22e6
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 02:27:33 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,43 +1,15 @@
-INSERT INTO series (title, author_id, subgenre_id) VALUES
-("A Song of Ice and Fire", 1, 1), ("Second Series", 2, 2);
+INSERT INTO series (id, title, author_id, subgenre_id) VALUES (1, "A Song of Ice and Fire", 1, 1), (2, "Second Series", 2, 2);
 
-INSERT INTO subgenres (name) VALUES
-("medieval"), ("space opera");
+INSERT INTO subgenres (id, name) VALUES (1, "medieval"), (2, "space opera");
 
-INSERT INTO authors (name) VALUES
-("George R. R. Martin"), ("Second Author");
+INSERT INTO authors (id, name) VALUES (1, "George R. R. Martin"), (2, "Second Author");
 
-INSERT INTO books (title, year, series_id) VALUES
-("Game of Thrones", 1996, 1), 
-("A Clash of Kings", 1998, 1), 
-("A Storm of Swords", 2000, 1), 
-("First Book", 2002, 2), 
-("Second Book", 2003, 2), 
-("Third Book", 2005, 2);
+INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 1996, 1), (2, "A Clash of Kings", 1998, 1), (3, "A Storm of Swords", 2000, 1), (4, "First Book", 2002, 2), (5, "Second Book", 2003, 2), (6, "Third Book", 2005, 2);
 
-INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
-("Character One", "motto one", "cylon", 2), 
-("Character Two", "motto two", "human", 2), 
-("Character Three", "motto three", "cylon", 2), 
-("Character Four", "motto four", "cylon", 2);
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1), (4, "Eddard Stark", "Winter is coming", "human", 1);
 
+INSERT INTO characters (id, name, motto, species, author_id) VALUES (5, "Character One", "motto one", "cylon", 2), (6, "Character Two", "motto two", "human", 2), (7, "Character Three", "motto three", "cylon", 2), (8, "Character Four", "motto four", "cylon", 2);
 
-INSERT INTO character_books (book_id, character_id) VALUES
-(1, 1), 
-(1, 2), 
-(2, 2), 
-(3, 2), 
-(1, 3), 
-(2, 3), 
-(3, 3), 
-(1, 4);
+INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
-INSERT INTO character_books (book_id, character_id) VALUES 
-(4, 5), 
-(4, 6), 
-(5, 6), 
-(6, 6), 
-(4, 7), 
-(5, 7), 
-(6, 7), 
-(4, 8);
+INSERT INTO character_books (id, book_id, character_id) VALUES (9, 4, 5), (10, 4, 6), (11, 5, 6), (12, 6, 6), (13, 4, 7), (14, 5, 7), (15, 6, 7), (16, 4, 8);
