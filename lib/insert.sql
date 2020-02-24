beacfcd0d91f136464886517daa1039cde18efdd
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 02:22:11 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -22,20 +22,22 @@ INSERT INTO characters (name, motto, species, author_id, series_id) VALUES
 ("Character Four", "motto four", "cylon", 2);
 
 
-INSERT INTO character_books (character_id, book_id) VALUES
-(1, 4),
-(1, 5),
-(1, 6),
-(2, 4),
-(2, 5),
-(2, 6),
-(3, 4),
-(4, 5),
-(5, 1),
-(5, 2),
-(5, 3),
-(6, 1),
-(6, 2),
-(6, 3),
-(7, 1),
-(8, 3);
+INSERT INTO character_books (book_id, character_id) VALUES
+(1, 1), 
+(1, 2), 
+(2, 2), 
+(3, 2), 
+(1, 3), 
+(2, 3), 
+(3, 3), 
+(1, 4);
+
+INSERT INTO character_books (book_id, character_id) VALUES 
+(4, 5), 
+(4, 6), 
+(5, 6), 
+(6, 6), 
+(4, 7), 
+(5, 7), 
+(6, 7), 
+(4, 8);
