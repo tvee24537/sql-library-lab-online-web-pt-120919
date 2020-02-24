ba4d8fbedaec1a09b0fba95e4cf2388d3c1b1277
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 02:02:50 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -4,7 +4,7 @@ end
 end
 
 def select_name_and_motto_of_char_with_longest_motto
-  "Write your SQL query here"
+  "SELECT characters.name, characters.motto FROM characters ORDER BY LENGTH(motto) DESC LIMIT 1;"
 end
 
 
