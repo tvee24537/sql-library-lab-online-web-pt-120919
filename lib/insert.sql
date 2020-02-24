0b6a0e79750012adc73ec006abb5579b1aad70c4
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 02:18:36 +0000

    Automatically backed up by Learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -2,7 +2,7 @@ INSERT INTO series (title, author_id, subgenre_id) VALUES
 ("A Song of Ice and Fire", 1, 1), ("Second Series", 2, 2);
 
 INSERT INTO subgenres (name) VALUES
-("Fantasy"), ("Magic");
+("medieval"), ("space opera");
 
 INSERT INTO authors (name) VALUES
 ("J.R.R. Tolkien"), ("J.K. Rowling");
