f97de7769e75302eb84923777ff272ae733eaea1
Thanapat Veerakajorn <t.veerakajorn@gmail.com>
2020-02-24 02:14:48 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,10 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT species, COUNT (*) FROM characters GROUP BY species ORDER BY COUNT (species) DESC LIMIT 1;"
+  "SELECT species, COUNT(species) AS num_species FROM characters
+    GROUP BY species
+    ORDER BY num_species DESC
+    LIMIT 1;"
 end
 
 def select_name_and_series_subgenres_of_authors
